1d091601cd3177f7225b5d19386cc3dd48c43982
trent <trent@DESKTOP-NKK8L7L>
2019-10-15 22:57:23 -0500

    Merge Trenton/master
    Conflicts:
    	src/main/java/salesTax.java

---
+++ src/main/java/com/mycompany/lab4/Car.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab4;
+
+/**
+ *
+ * @author trent
+ */
+public class Car {
+    public int yearModel;
+    public string make;
+    public int speed;
+    
+    public Constcutor()
+    {
+    
+    }
+    
+    public void main(String[] args) {
+        
+    }
+    
+}
